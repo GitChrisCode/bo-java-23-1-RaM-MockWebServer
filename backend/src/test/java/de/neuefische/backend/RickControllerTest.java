a841d4f84ab7f80890c8e1ca82dae1047bf5086b
GitChrisCode <chrha.gw2@gmail.com>
2023-06-06 15:43:22 +0200

    fixed the test

--- backend/src/test/java/de/neuefische/backend/RickControllerTest.java
+++ backend/src/test/java/de/neuefische/backend/RickControllerTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
@@ -46,19 +48,8 @@ class RickControllerTest {
     void getRickCharacters() throws Exception {
 
         mockWebServer.enqueue(new MockResponse()
-
-                .setBody("""
-                        {
-                          "results": [
-                            {
-                              "id": "1",
-                              "name": "Currywurst",
-                              "status": "Alive",
-                              "species": "Human"
-                            }
-                          ]
-                        }
-                        """));
+                        .setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+                .setBody("{\"results\": [{\"id\": \"1\", \"name\": \"Currywurst\", \"status\": \"Alive\", \"species\": \"Human\"}]}"));
 
 
         mvc.perform(get("/api/rick-and-morty/characters"))
